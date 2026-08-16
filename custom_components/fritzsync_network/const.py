ac103627cf626396207ea6a76b901636c461f1f4
Marcin Kulmaczewski <84371906+kulmi84@users.noreply.github.com>
2026-08-16 17:33:39 +0200

    Ordne Pi-hole-Einträge den Netzfiltern zu

--- custom_components/fritzsync_network/const.py
+++ custom_components/fritzsync_network/const.py
@@ -6,7 +6,7 @@ from homeassistant.const import Platform
 
 DOMAIN: Final = "fritzsync_network"
 MANUFACTURER: Final = "FRITZ!"
-VERSION: Final = "1.9.3"
+VERSION: Final = "1.9.4"
 
 PLATFORMS: Final = [Platform.SENSOR]
 
