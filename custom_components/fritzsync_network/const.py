bbb71882aa831a1a7ba15c281ea5ba01922fe64c
Marcin Kulmaczewski <84371906+kulmi84@users.noreply.github.com>
2026-08-16 18:16:24 +0200

    Fix Excel, columns and DNS entry (custom_components/fritzsync_network/const.py)

--- custom_components/fritzsync_network/const.py
+++ custom_components/fritzsync_network/const.py
@@ -6,7 +6,7 @@ from homeassistant.const import Platform
 
 DOMAIN: Final = "fritzsync_network"
 MANUFACTURER: Final = "FRITZ!"
-VERSION: Final = "1.10.1"
+VERSION: Final = "1.10.2"
 
 PLATFORMS: Final = [Platform.SENSOR]
 
