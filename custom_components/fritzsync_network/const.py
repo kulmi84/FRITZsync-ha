861140e5ad456a28e233a18fa1e5c8dc769da786
Marcin Kulmaczewski <84371906+kulmi84@users.noreply.github.com>
2026-08-16 18:01:22 +0200

    Fix WebUI master source (custom_components/fritzsync_network/const.py)

--- custom_components/fritzsync_network/const.py
+++ custom_components/fritzsync_network/const.py
@@ -6,7 +6,7 @@ from homeassistant.const import Platform
 
 DOMAIN: Final = "fritzsync_network"
 MANUFACTURER: Final = "FRITZ!"
-VERSION: Final = "1.10.0"
+VERSION: Final = "1.10.1"
 
 PLATFORMS: Final = [Platform.SENSOR]
 
