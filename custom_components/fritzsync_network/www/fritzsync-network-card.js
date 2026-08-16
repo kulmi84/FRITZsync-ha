a77524503b4cb93b2ed30e5c80ab84d1c5aedea0
Marcin Kulmaczewski <84371906+kulmi84@users.noreply.github.com>
2026-08-16 21:28:16 +0200

    Alle PTR-Hostdatensätze derselben IP aktualisieren

--- custom_components/fritzsync_network/www/fritzsync-network-card.js
+++ custom_components/fritzsync_network/www/fritzsync-network-card.js
@@ -17,7 +17,7 @@
  *   eingebundenes Modul beim zweiten define() abbricht.
  */
 
-const FBN_VERSION = "1.10.13";
+const FBN_VERSION = "1.10.14";
 
 /* ------------------------------------------------------------------ */
 /* Konfiguration                                                       */
