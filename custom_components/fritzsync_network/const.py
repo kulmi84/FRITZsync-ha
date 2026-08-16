30f7e93f5978ad9f8ce4406417b8e3f8688a6a36
Marcin Kulmaczewski <84371906+kulmi84@users.noreply.github.com>
2026-08-16 20:12:08 +0200

    ui: show progress during Pi-hole operations

--- custom_components/fritzsync_network/const.py
+++ custom_components/fritzsync_network/const.py
@@ -6,7 +6,7 @@ from homeassistant.const import Platform
 
 DOMAIN: Final = "fritzsync_network"
 MANUFACTURER: Final = "FRITZ!"
-VERSION: Final = "1.10.9"
+VERSION: Final = "1.10.10"
 
 PLATFORMS: Final = [Platform.SENSOR]
 
