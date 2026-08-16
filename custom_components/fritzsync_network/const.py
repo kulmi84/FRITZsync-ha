279bc633ba639499d23892082cef17c0777eae86
Marcin Kulmaczewski <84371906+kulmi84@users.noreply.github.com>
2026-08-16 20:47:36 +0200

    PTR1 aus Pi-hole-DNS aktualisieren

--- custom_components/fritzsync_network/const.py
+++ custom_components/fritzsync_network/const.py
@@ -6,7 +6,7 @@ from homeassistant.const import Platform
 
 DOMAIN: Final = "fritzsync_network"
 MANUFACTURER: Final = "FRITZ!"
-VERSION: Final = "1.10.11"
+VERSION: Final = "1.10.12"
 
 PLATFORMS: Final = [Platform.SENSOR]
 
