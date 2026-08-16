a103754034138cb8d9c1b38a2ffe71d5359b7153
Marcin Kulmaczewski <84371906+kulmi84@users.noreply.github.com>
2026-08-16 18:45:11 +0200

    release: prepare 1.10.4

--- custom_components/fritzsync_network/const.py
+++ custom_components/fritzsync_network/const.py
@@ -6,7 +6,7 @@ from homeassistant.const import Platform
 
 DOMAIN: Final = "fritzsync_network"
 MANUFACTURER: Final = "FRITZ!"
-VERSION: Final = "1.10.3"
+VERSION: Final = "1.10.4"
 
 PLATFORMS: Final = [Platform.SENSOR]
 
