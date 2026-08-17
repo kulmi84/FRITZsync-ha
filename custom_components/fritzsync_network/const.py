0db7173e3ed9c7184cf394f480122f196292e837
Marcin Kulmaczewski <84371906+kulmi84@users.noreply.github.com>
2026-08-17 14:51:50 +0200

    Standardfilter nach Dashboard-Wechsel wiederherstellen (#32)
    Setzt den konfigurierten Standardfilter auch bei zwischengespeicherten Lovelace-Karten nach Navigation zurück. Version 1.10.22.

--- custom_components/fritzsync_network/const.py
+++ custom_components/fritzsync_network/const.py
@@ -6,7 +6,7 @@ from homeassistant.const import Platform
 
 DOMAIN: Final = "fritzsync_network"
 MANUFACTURER: Final = "FRITZ!"
-VERSION: Final = "1.10.21"
+VERSION: Final = "1.10.22"
 
 PLATFORMS: Final = [Platform.SENSOR]
 
