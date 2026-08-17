f47dfe7da1b8f2c1167125d8c5ccbb4a800afc75
Marcin Kulmaczewski <84371906+kulmi84@users.noreply.github.com>
2026-08-17 20:20:35 +0200

    Version auf 1.10.31 setzen

--- custom_components/fritzsync_network/const.py
+++ custom_components/fritzsync_network/const.py
@@ -6,7 +6,7 @@ from homeassistant.const import Platform
 
 DOMAIN: Final = "fritzsync_network"
 MANUFACTURER: Final = "FRITZ!"
-VERSION: Final = "1.10.30"
+VERSION: Final = "1.10.31"
 
 PLATFORMS: Final = [Platform.SENSOR]
 
