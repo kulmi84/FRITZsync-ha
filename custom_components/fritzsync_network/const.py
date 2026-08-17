4dc478b3ab262a3370676fcf571d5d11c6a955a4
Marcin Kulmaczewski <84371906+kulmi84@users.noreply.github.com>
2026-08-17 15:31:50 +0200

    Bump and document version 1.10.25

--- custom_components/fritzsync_network/const.py
+++ custom_components/fritzsync_network/const.py
@@ -6,7 +6,7 @@ from homeassistant.const import Platform
 
 DOMAIN: Final = "fritzsync_network"
 MANUFACTURER: Final = "FRITZ!"
-VERSION: Final = "1.10.24"
+VERSION: Final = "1.10.25"
 
 PLATFORMS: Final = [Platform.SENSOR]
 
