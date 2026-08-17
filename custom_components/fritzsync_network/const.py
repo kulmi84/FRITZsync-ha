2899654b377094dba35fff7fa7747d8d73c33ac5
Marcin Kulmaczewski <84371906+kulmi84@users.noreply.github.com>
2026-08-17 12:54:24 +0200

    Doppelte WebUI-Geräteidentitäten je IPv4 bereinigen

--- custom_components/fritzsync_network/const.py
+++ custom_components/fritzsync_network/const.py
@@ -6,7 +6,7 @@ from homeassistant.const import Platform
 
 DOMAIN: Final = "fritzsync_network"
 MANUFACTURER: Final = "FRITZ!"
-VERSION: Final = "1.10.17"
+VERSION: Final = "1.10.18"
 
 PLATFORMS: Final = [Platform.SENSOR]
 
