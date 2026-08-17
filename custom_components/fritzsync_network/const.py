9b06a26f09e33417ccf5c73bd9d7afab810f7b7c
Marcin Kulmaczewski <84371906+kulmi84@users.noreply.github.com>
2026-08-17 18:59:32 +0200

    Use Home Assistant select for default filter (1.10.28)

--- custom_components/fritzsync_network/const.py
+++ custom_components/fritzsync_network/const.py
@@ -6,7 +6,7 @@ from homeassistant.const import Platform
 
 DOMAIN: Final = "fritzsync_network"
 MANUFACTURER: Final = "FRITZ!"
-VERSION: Final = "1.10.27"
+VERSION: Final = "1.10.28"
 
 PLATFORMS: Final = [Platform.SENSOR]
 
