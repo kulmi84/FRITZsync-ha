782b817f354c48e2fa0d060062c85e8b99c71029
Marcin Kulmaczewski <84371906+kulmi84@users.noreply.github.com>
2026-08-17 18:42:23 +0200

    Standardfilter zuverlässig speichern (1.10.27)
    Behebt die Filterpersistenz und die Darstellung der Standardfilter-Auswahl im Dark Mode.

--- custom_components/fritzsync_network/const.py
+++ custom_components/fritzsync_network/const.py
@@ -6,7 +6,7 @@ from homeassistant.const import Platform
 
 DOMAIN: Final = "fritzsync_network"
 MANUFACTURER: Final = "FRITZ!"
-VERSION: Final = "1.10.25"
+VERSION: Final = "1.10.27"
 
 PLATFORMS: Final = [Platform.SENSOR]
 
